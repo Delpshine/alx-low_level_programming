33915936cac09ebc3bce079368e42fdd4c211d00
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-04-23 18:44:07 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+
+}
