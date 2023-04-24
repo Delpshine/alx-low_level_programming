9b5b6f95a061430c6f81dc0b775edc720294f38b
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-04-23 18:08:43 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
