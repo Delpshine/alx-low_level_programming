bb8579d50fde90ae825ac03fef228833e0924379
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-04-26 16:15:02 -0700

    update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,31 +1,21 @@
 #include "main.h"
 /**
  * jack_bauer - prints the minutes of a day
- *
- * Return: no retirn
  */
 void jack_baurer(void)
 {
-	int a, b, c, d;
+	int hour, minutes;
 
-	for (a = 0; a <= 2; a++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (minutes = 0; minutes <= 59; minutes++)
 		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					if (a >= 2 && b >= 4)
-						break;
-					_putchar(a + 48);
-					_putchar(b + 48);
-					_putchar(58);
-					_putchar(c + 48);
-					_putchar(d + 48);
-					_putchar('\n');
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
