38649ab75df7597a2e669589fd4b3b949483e789
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-04-26 03:09:26 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry
+ * Prints _putchar
+ * Return: Always (0)
+ */
+int main(void)
+{
+	-putchar('-');
+	-putchar('p');
+	-putchar('u');
+	-putchar('t');
+	-Putchar('c');
+	-putchar('h');
+	-putchar('a');
+	-putchar('r');
+	-putchar('\n');
+	return (0);
+}
