90aef7078f47dc932b0f102a5ee011d81861aacf
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-04-26 12:25:14 -0700

    task 4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,20 @@
 /**
  * main - Entry
  * print_alphabet - Print all alphabet in lowercase
+ *
  * Return: Always (0)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++
+)
+	{
 
 		_putchar(letter);
+
+	}
 
 	_putchar('\n');
 }
