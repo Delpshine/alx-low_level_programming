39f7b297bd1a206ba07a011a8c6b45233cb1e9e5
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-04-26 08:29:58 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check thr code
- *
- * Return:Always (0)
+ * main - Entry
+ * print_alphabet - print all alphabet in lowercase
+ * Return: Always (0)
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+		_putchar(letter);
+
+	_putchar('\n');
 }
+
+
