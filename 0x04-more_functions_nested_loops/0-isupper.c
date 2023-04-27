dc2dab007f61961b8d940e8f25f7a9bfd1e36b54
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-04-27 15:04:14 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isupper - checks if is an uppercase character.
+ * _isupper - checks if parameter is an uppercase character.
  * @c: input character.
- * Return: 1 if is an uppercase character,
+ * Return: 1 if an uppercase character,
  * 0 in other case.
  */
 int _isupper(int c)
