d02451315dd5a9f85afb7a7213fcfab10282237b
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-05-02 21:52:34 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,12 @@
 
 int _strlen(char *s)
 {
-	int p = 0;
+	int longi = 0;
 
-	incremeant up to when the last character is NULL,\0/
-		while (*(s + p) != 0)
-		{
-			 p++;
-		}
-	return (p);
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
 }
