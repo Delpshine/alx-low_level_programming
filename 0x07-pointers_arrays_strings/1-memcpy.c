82338a36d24625f5b6e28bdd71142c08b342ebf6
Philadelphia Olawale <philadelphiasamuel@gmail.com>
2023-05-09 21:40:01 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /*
- * -memcpy.c - a function that copies memory area
+ * _memcpy.c - a function that copies memory area
  *  @dest: memory where its stored
  *  @src: memory from where it is copied
  *  @n: number of bytes
  *
- *  Return: copied memory with changed n bytes
+ *  Return: copied memory with n bytes changed
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
