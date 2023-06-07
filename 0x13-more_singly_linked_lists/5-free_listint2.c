dd4bac664adec01f9aab8bd2e93bed52ecb7016e
philadelphia olawale <philadelphiasamuel@gmail.com>
2023-06-07 03:21:43 -0700

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,13 @@
 #include "lists.h"
 
-
 /**
-
  * free_listint2 - frees a linked list
-
  * @head: pointer to the listint_t list to be freed
-
  */
-
 void free_listint2(listint_t **head)
 
 {
-
-        listint_t *temp;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
